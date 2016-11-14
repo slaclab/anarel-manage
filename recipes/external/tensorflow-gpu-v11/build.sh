f4843c7b004d0fabbd5958a0847a7a0988542962
David Schneider <davidsch@slac.stanford.edu>
2016-11-14 09:03:38 -0800

    anarel.yaml has numba and some other packages that looked good from anaconda

---
+++ recipes/external/tensorflow-gpu-v11/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install using pip from the whl file provided by Google
+
+if [ `uname` == Linux ]; then
+    if [ "$PY_VER" == "2.7" ]; then
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl
+    elif [ "$PY_VER" == "3.4" ]; then
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0-cp34-cp34m-linux_x86_64.whl
+    elif [ "$PY_VER" == "3.5" ]; then
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0-cp35-cp35m-linux_x86_64.whl
+    fi
+fi
