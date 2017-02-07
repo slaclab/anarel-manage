576c1aa522c5f880d34996eaca20094dc4539d94
David Schneider <davidsch@slac.stanford.edu>
2017-02-07 11:25:12 -0800

    change default email to ana mailing list, extend anaconda upload to control which of the 3 channels to use

--- recipes/external/tensorflow-gpu-v12-cudnn-v5-cuda8/build.sh
+++ recipes/external/tensorflow-gpu-v12-cudnn-v5-cuda8/build.sh
@@ -4,10 +4,10 @@
 
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0-cp27-none-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp27-none-linux_x86_64.whl
     elif [ "$PY_VER" == "3.4" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0-cp34-cp34m-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp34-cp34m-linux_x86_64.whl
     elif [ "$PY_VER" == "3.5" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0-cp35-cp35m-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
     fi
 fi
