8d47621b355a99593186d84bf42a9792b42661ac
David Schneider <davidsch@slac.stanford.edu>
2017-04-27 22:51:09 -0700

    update to tensorflow 1.1.0

--- recipes/external/tensorflow/build.sh
+++ recipes/external/tensorflow/build.sh
@@ -4,12 +4,12 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps tensorflow==1.0.1
+        pip install --no-deps tensorflow==1.1.0
     else
-        pip3 install --no-deps tensorflow==1.0.1
+        pip3 install --no-deps tensorflow==1.1.0
     fi
 fi
 
 if [ `uname` == Linux ]; then
-    pip install --no-deps tensorflow==1.0.1
+    pip install --no-deps tensorflow==1.1.0
 fi
