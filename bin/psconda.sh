74a2cf905a7dac20c1ccce12948623f403977734
David Schneider <davidsch@slac.stanford.edu>
2020-05-21 21:03:13 -0700

    psconda.sh for new py2/py3 psana1

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,5 +1,8 @@
-ana_current=$(head -n 1 /reg/g/psdm/sw/conda1/current/ana/ana-current)
-rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-export PATH=/reg/g/psdm/sw/conda1/manage/bin:$PATH
-. /reg/g/psdm/sw/conda1/inst/miniconda3-prod-rhel$rhel_version/etc/profile.d/conda.sh
-conda activate $ana_current
+# needed to avoid file locking crash in mpi splitscan tests
+export HDF5_USE_FILE_LOCKING=FALSE
+export SIT_ROOT=/reg/g/psdm
+export SIT_ARCH=x86_64-rhel7-gcc48-opt
+eval "$(/reg/g/psdm/sw/conda1/inst/miniconda2-prod-rhel7/bin/conda shell.bash hook)"
+#ana_current=$(head -n 1 /reg/g/psdm/sw/conda1/current/ana/ana-current)
+#rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
+#conda activate $ana_current
