00b219fc7f81d2ebdf4dcfd4a6e15a6b2d5dee41
Christopher O'Grady <cpo@slac.stanford.edu>
2020-05-27 17:38:18 -0700

    change psconda.sh to support py2 and py3

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -3,6 +3,4 @@ export HDF5_USE_FILE_LOCKING=FALSE
 export SIT_ROOT=/reg/g/psdm
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
 eval "$(/reg/g/psdm/sw/conda1/inst/miniconda2-prod-rhel7/bin/conda shell.bash hook)"
-#ana_current=$(head -n 1 /reg/g/psdm/sw/conda1/current/ana/ana-current)
-#rhel_version=$(less /etc/redhat-release | grep -o -E '[0-9]+' | head -1)
-#conda activate $ana_current
+conda activate ana-2.0.3$@
