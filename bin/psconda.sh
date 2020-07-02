a775deb50d1eb8c0508861d56803d5a5393b9020
Valerio Mariani <valmar@slac.stanford.edu>
2020-07-02 15:56:51 +0000

    Bump version to 2.0.8

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -3,4 +3,4 @@ export HDF5_USE_FILE_LOCKING=FALSE
 export SIT_ROOT=/reg/g/psdm
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
 eval "$(/reg/g/psdm/sw/conda1/inst/miniconda2-prod-rhel7/bin/conda shell.bash hook)"
-conda activate ana-2.0.4$@
+conda activate ana-2.0.8$@
