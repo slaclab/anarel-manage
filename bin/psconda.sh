b7a11d1ee9872e98652f99afc57388534e42c1a9
Valerio Mariani <valmar@slac.stanford.edu>
2021-03-24 17:27:35 -0700

    update psconda.sh

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -2,5 +2,15 @@
 export HDF5_USE_FILE_LOCKING=FALSE
 export SIT_ROOT=/cds/sw/ds/ana/
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
+# needed for SRCF
+export OPENBLAS_NUM_THREADS=1
 eval "$(/cds/sw/ds/ana/conda1/inst/bin/conda shell.bash hook)"
-conda activate ana-4.0.5$@
+ext=""
+for arg in "$@"
+do
+  if [ "$arg" == "-py3" ]
+  then
+     ext="-py3"
+  fi
+done
+conda activate ana-4.0.20$ext
