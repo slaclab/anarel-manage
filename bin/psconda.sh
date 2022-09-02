ee87426efd54eeed7e4b64df6b063f42039d5e00
Valerio Mariani <valmar@slac.stanford.edu>
2022-09-02 12:48:37 -0700

    Added support for conda v2 installation

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -4,13 +4,24 @@ export SIT_ROOT=/cds/sw/ds/ana/
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
 # needed for SRCF
 export OPENBLAS_NUM_THREADS=1
-eval "$(/cds/sw/ds/ana/conda1/inst/bin/conda shell.bash hook)"
 ext=""
+v2=0
 for arg in "$@"
 do
   if [ "$arg" == "-py3" ]
   then
      ext="-py3"
   fi
+  if [ "$arg" == "-v2" ]
+  then
+     v2=1
+  fi
 done
-conda activate ana-4.0.36$ext
+if [ $v2 -eq 1 ]
+then
+  eval "$(/cds/sw/ds/ana/conda1-v2/inst/bin/conda shell.bash hook)"
+  export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda1/inst/envs/
+else
+  eval "$(/cds/sw/ds/ana/conda1/inst/bin/conda shell.bash hook)"
+fi
+conda activate ana-4.0.43$ext
