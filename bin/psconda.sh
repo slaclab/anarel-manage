8b4bf95af9ca26a0b447d40d2e181c90de170d6c
David A Schneider <psreldev@sdflogin001.sdf.slac.stanford.edu>
2023-10-23 11:40:22 -0700

    Updated conda.sh

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,27 +1,27 @@
 # needed to avoid file locking crash in mpi splitscan tests
 export HDF5_USE_FILE_LOCKING=FALSE
-export SIT_ROOT=/cds/sw/ds/ana/
+export SIT_ROOT=/sdf/group/lcls/ds/ana/
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
+export SIT_PSDM_DATA=/sdf/data/lcls/ds/
 # needed for SRCF
 export OPENBLAS_NUM_THREADS=1
-ext=""
-v2=0
+py2=0
 for arg in "$@"
 do
-  if [ "$arg" == "-py3" ]
+  if [ "$arg" == "-py2" ]
   then
-     ext="-py3"
-  fi
-  if [ "$arg" == "-v2" ]
-  then
-     v2=1
+     py2=1
   fi
 done
-if [ $v2 -eq 1 ]
-then
-  eval "$(/cds/sw/ds/ana/conda1-v2/inst/bin/conda shell.bash hook)"
-  export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda1/inst/envs/
+eval "$(/sdf/group/lcls/ds/ana/sw/conda1/inst/bin/conda shell.bash hook)"
+if [ $py2 -eq 1 ]
+then 
+  export SIT_DATA=/sdf/group/lcls/ds/ana/sw/conda1/inst/envs/ana-4.0.45/data:/sdf/group/lcls/ds/ana/data/
+  conda activate ana-4.0.45
 else
-  eval "$(/cds/sw/ds/ana/conda1/inst/bin/conda shell.bash hook)"
+  export SIT_DATA=/sdf/group/lcls/ds/ana/sw/conda1/inst/envs/ana-4.0.54-py3/data:/sdf/group/lcls/ds/ana/data/
+  conda activate ana-4.0.54-py3
 fi
-conda activate ana-4.0.44$ext
+
+#export SLURM_PRIORITY_RESERVATION=lcls:xppc00121_25
+export SLURM_PRIORITY_RESERVATION=lcls:onshift
