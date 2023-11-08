eef8993b1aaab12743c68aef674952607b014603
David A Schneider <psreldev@sdflogin002.sdf.slac.stanford.edu>
2023-11-07 17:57:13 -0800

    switch to psconda.sh that works in both s3df and old psana

--- bin/psconda.sh
+++ bin/psconda.sh
@@ -1,8 +1,22 @@
 # needed to avoid file locking crash in mpi splitscan tests
 export HDF5_USE_FILE_LOCKING=FALSE
-export SIT_ROOT=/sdf/group/lcls/ds/ana/
 export SIT_ARCH=x86_64-rhel7-gcc48-opt
-export SIT_PSDM_DATA=/sdf/data/lcls/ds/
+
+if [ -d "/sdf/group/lcls/" ]
+then
+    # s3df
+    export SIT_ROOT=/sdf/group/lcls/ds/ana
+    export SIT_PSDM_DATA=/sdf/data/lcls/ds
+    eval "$(/sdf/group/lcls/ds/ana/sw/conda1/inst/bin/conda shell.bash hook)"
+    export CONDA_ENVS_DIRS=/sdf/group/lcls/ds/ana/sw/conda1/inst/envs
+else
+    # psana
+    export SIT_ROOT=/cds/group/psdm
+    export SIT_PSDM_DATA=/cds/data/psdm
+    eval "$(/cds/sw/ds/ana/conda1-v2/inst/bin/conda shell.bash hook)"
+    export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda1/inst/envs
+fi
+
 # needed for SRCF
 export OPENBLAS_NUM_THREADS=1
 py2=0
@@ -13,15 +27,12 @@ do
      py2=1
   fi
 done
-eval "$(/sdf/group/lcls/ds/ana/sw/conda1/inst/bin/conda shell.bash hook)"
+
 if [ $py2 -eq 1 ]
 then 
-  export SIT_DATA=/sdf/group/lcls/ds/ana/sw/conda1/inst/envs/ana-4.0.45/data:/sdf/group/lcls/ds/ana/data/
+  export SIT_DATA=$CONDA_ENVS_DIRS/ana-4.0.45/data:$SIT_ROOT/data/
   conda activate ana-4.0.45
 else
-  export SIT_DATA=/sdf/group/lcls/ds/ana/sw/conda1/inst/envs/ana-4.0.54-py3/data:/sdf/group/lcls/ds/ana/data/
+  export SIT_DATA=$CONDA_ENVS_DIRS/ana-4.0.54-py3/data:$SIT_ROOT/data/
   conda activate ana-4.0.54-py3
 fi
-
-#export SLURM_PRIORITY_RESERVATION=lcls:xppc00121_25
-export SLURM_PRIORITY_RESERVATION=lcls:onshift
